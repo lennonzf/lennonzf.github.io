adb0e8e5153236828977843db7be6ab9e4316cf2
lennonzf <lennon9469@gmail.com>
2016-10-06 17:16:59 +1000

    add bower and sticky-kit

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -1,6 +1,14 @@
+// bower components
+//= require jquery
+//= require sticky-kit
+
 //= require smooth_scroll
 //= require prism
 //= require prism_line_numbers
 
+
 // initialize smoothScroll
 smoothScroll.init();
+
+// sticky-kit for style-guide
+$("#side_navigation").stick_in_parent();
