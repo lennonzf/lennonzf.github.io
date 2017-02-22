bdb2f2375a6bbcc17cd6934402e1ebf15c2562cc
Lennon Cheng <lennon9469@gmail.com>
2017-02-22 14:25:56 +1000

    add headroom.js but not getting navigation work yet

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -6,9 +6,17 @@
 //= require prism
 //= require prism_line_numbers
 
+//= require headroom
 
 // initialize smoothScroll
 smoothScroll.init();
 
 // sticky-kit for style-guide
 $("#side_navigation").stick_in_parent();
+
+// Initialise headroom.js
+document.addEventListener("DOMContentLoaded", function(event) {
+  var nav = document.querySelector(".container");
+  var headroom  = new Headroom(nav);
+  headroom.init();
+});
